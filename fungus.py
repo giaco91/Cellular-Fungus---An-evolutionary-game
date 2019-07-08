8613a38590a87dddaed0fb94eb5f410ef753d695
giaco <muzlu@hotmail.com>
2019-07-08 16:51:32 +0200

    new maps, new game

--- fungus.py
+++ fungus.py
@@ -2,39 +2,50 @@ import random
 import numpy as np
 from common_utils import *
 
+def sigmoid(s):
+	return 1/(1+np.exp(-s))
+
 class Fungus():
-    def __init__(self,gene=None,n_types=3):
+    def __init__(self,gene=None,n_types=4,n_space=3,n_states=2,sig=-1):
     	self.n_types=n_types
+    	self.n_space=n_space
+    	self.n_states=n_states
     	self.score=0
     	self.alive=True
     	self.size=None
+    	self.sig=sig
+    	prob=sigmoid(sig)#the marginal probability to create a living cell
+    	self.p=np.zeros(2)
+    	self.p[0]=1-prob
+    	self.p[1]=prob
     	self.life_frames=[]#here we store the played frames
     	if gene is not None:
     		self.gene=gene
     	else:
-    		p=[0.8,0.2]
-    		gene=np.zeros((n_types,n_types,n_types,n_types,n_types,n_types,n_types,n_types,n_types)).astype(int)
+    		gene=np.zeros((n_types,n_types,n_types,n_types,n_space,n_types,n_types,n_types,n_types)).astype(int)
     		for i1 in range(n_types):
     			for i2 in range(n_types):
     				for i3 in range(n_types):
     					for i4 in range(n_types):
-    						for i5 in range(n_types):
+    						for i5 in range(n_space):
     							for i6 in range(n_types):
     								for i7 in range(n_types):
     									for i8 in range(n_types):
     										for i9 in range(n_types):
-    											gene[i1,i2,i3,i4,i5,i6,i7,i8,i9]=np.random.choice(2, 1, p=p)
+    											gene[i1,i2,i3,i4,i5,i6,i7,i8,i9]=int(np.random.choice(n_states, 1, p=self.p))
     		self.gene=gene
-
 
     def get_mutation_gene(self,N=1):
     	mut_gene=np.copy(self.gene)
     	for n in range(N):
-	    	i=np.random.randint(self.n_types,size=9)
-	    	idx=(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8])
-	    	mut_gene[idx]=-mut_gene[idx]+1
-    	# print(np.array_equal(mut_gene,self.gene))
-    	return mut_gene
+	    	i=np.random.randint(self.n_types,size=8)
+	    	j=np.random.randint(self.n_space,size=1)
+	    	idx=(i[0],i[1],i[2],i[3],j[0],i[4],i[5],i[6],i[7])
+	    	#mut_gene[idx]=-mut_gene[idx]+1 #flip mutation
+	    	mut_gene[idx]=np.random.choice(self.n_states,1,p=self.p)
+    	# print(np.sum(mut_gene!=self.gene))
+    	self.sig+=(2*np.random.randint(2,size=1)-1)*(0.1*np.abs(self.sig)+0.1)
+    	return mut_gene,self.sig
 
 class Game():
 	def __init__(self,idx_map,fungus,save_frames=False,max_size=1000):
@@ -108,16 +119,56 @@ class Game():
 				print('the fungus died!')
 				return self.n_iter
 		self.fungus.life_frames=self.frame_list
-		self.fungus.size=(new_idx_map == 1).sum()
+		self.fungus.size=(self.frame_list[-1] == 1).sum()
+		self.fungus.score=((self.frame_list[0]==3).sum()-(self.frame_list[-1]==3).sum()+10)/((self.frame_list[-1]== 1).sum()+10)
 		return self.n_iter		
 
+	def play_green_is_life(self,max_n_iter=10000,die_fac=1000):
+		s=self.frame_list[-1].shape
+		mass_integrator=0
+		score=0
+		while self.n_iter<max_n_iter:
+			current_idx_map=self.frame_list[-1]
+			current_size=(current_idx_map == 1).sum()
+			mass_integrator+=current_size
+			current_grass_eaten=((self.frame_list[0]==3).sum()-(self.frame_list[-1]==3).sum())+10
+			score=current_grass_eaten**1.5/mass_integrator
+			#print('current grass eaten='+str(current_grass_eaten)+'-- mass integrator='+str(mass_integrator))
+			p_die=2*(1-sigmoid(die_fac*score))#probability to die for any living cell
+			prob=[p_die,1-p_die]
+			new_idx_map=np.copy(current_idx_map)
+			for i in range(1,s[0]-1):
+				for j in range(1,s[1]-1):
+					#living space condition
+					if current_idx_map[i,j] ==0 or current_idx_map[i,j] ==1 or current_idx_map[i,j] ==3:
+						nh=current_idx_map[i-1:i+2,j-1:j+2].flatten().astype(int)
+						if current_idx_map[i,j]==1:
+							current_idx_map[i,j]=int(np.random.choice(2, 1, p=prob))
+						if 1 in nh:
+							if nh[4]==3:
+								nh[4]=2
+							new_idx_map[i,j]=self.fungus.gene[nh[0],nh[1],nh[2],nh[3],nh[4],nh[5],nh[6],nh[7],nh[8]]
+			if not self.save_frames:
+				del self.frame_list[-1]
+			self.frame_list.append(new_idx_map)
+			self.n_iter+=1
+			if current_size==0 or np.array_equal(self.frame_list[-1],self.frame_list[-2]):
+				self.fungus.life_frames=self.frame_list
+				self.fungus.size=current_size
+				self.fungus.alive=False
+				print('the fungus died!')
+				return self.n_iter
+		self.fungus.life_frames=self.frame_list
+		self.fungus.size=(new_idx_map == 1).sum()
+		self.fungus.score=score
+		return self.n_iter	
 
 def competition(population,idx_map,max_n_iter=200):
 	i=1
 	for fungus in population:
 		print('play-round of fungus: '+str(i))
-		n_iter=Game(idx_map,fungus,save_frames=True).play_no_win(max_n_iter=max_n_iter)
-		fungus.score=((fungus.life_frames[0]==3).sum()-(fungus.life_frames[-1]==3).sum()+1)/(fungus.size+1)
+		n_iter=Game(idx_map,fungus,save_frames=True).play_green_is_life(max_n_iter=max_n_iter)
+		#fungus.score=((fungus.life_frames[0]==3).sum()-(fungus.life_frames[-1]==3).sum()+10)/(fungus.size+10)
 		make_gif(fungus.life_frames)
 		i+=1
 
@@ -148,10 +199,10 @@ def mutation(ancestors,N,n_mutations=100):
 	n=0
 	descendants=[]
 	while n<=N-1:
-		mutated_gene=ancestors[np.mod(n,L)].get_mutation_gene(N=n_mutations)
-		descendants.append(Fungus(gene=mutated_gene))
+		mutated_gene,mutated_s=ancestors[np.mod(n,L)].get_mutation_gene(N=n_mutations)
+		descendants.append(Fungus(gene=mutated_gene,sig=mutated_s))
 		n+=1
-	return(descendants)
+	return descendants
 
 def make_gif(life_frames,path='best_fungus.gif'):
 	frames=[]
@@ -162,7 +213,7 @@ def make_gif(life_frames,path='best_fungus.gif'):
 	frames[0].save(path,
 	               save_all=True,
 	               append_images=frames[1:],
-	               duration=150,
+	               duration=100,
 	               loop=0)
 
 
--- test.py
+++ test.py
@@ -3,67 +3,86 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
 import random
-import pickle 
+import pickle
+import argparse
+
 from common_utils import *
 from fungus import *
 
-img_path='maps/'
-map=5
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--map_path', default='maps/', help='path to map')
+parser.add_argument('--gif_path', default='gifs/', help='path where gifs should be stored')
+parser.add_argument('--winner_path', default='selected_pop/', help='where to store the best fungus subpopulation')
+parser.add_argument('--map', type=int, help='which map do you want to play?', default=6)
+parser.add_argument('--n_pop', type=int, help='size of population?', default=2)
+parser.add_argument('--n_survive', type=int, help='number of selected subpopulation?', default=1)
+parser.add_argument('--n_gen', type=int, help='how many generations?', default=3)
+parser.add_argument('--n_mut', type=int, help='how many point mutations?', default=1000)
+parser.add_argument('--n_iter', type=int, help='how many iterations per game?', default=20)
+parser.add_argument('--load_pop', type=str2bool, help='do you want to load a saved popuplation?', default=False)
 
 
+opt = parser.parse_args()
+print(opt)
 
 # new_im=create_image(256,256)
 # new_im.save(img_path+'map7.png', 'png')
 # raise ValueError('')
 
 
-
 #load images
-img_list=[]
+map_list=[]
 i=0
-for img_name in os.listdir(img_path):
-  if img_name.endswith(".jpg") or img_name.endswith(".png"):
-    img_list.append(pil_to_np(load(img_path+img_name).convert("RGB")))
+for map_name in os.listdir(opt.map_path):
+  if map_name.endswith(".jpg") or map_name.endswith(".png"):
+    map_list.append(pil_to_np(load(opt.map_path+map_name).convert("RGB")))
     i+=1
 # np_to_pil(img_list[0]).show()
-idx_map=npimg_to_int_array(img_list[map-1])
-
-
-N=3#size of population
-n=1#size of selected subpop
-n_mutations=100
-num_competitions=3
-load_pop=False
-max_n_iter=20
+idx_map=npimg_to_int_array(map_list[opt.map-1])
 
 #--initialize first population
-if load_pop:
-	with open('best_subpop.pkl', 'rb') as f:
+if opt.load_pop:
+	print('load old generation')
+	with open(opt.winner_path+'best_subpop_map='+str(opt.map)+'.pkl', 'rb') as f:
 	    best_subpop = pickle.load(f)
-	population=mutation(best_subpop,N)
+	population=mutation(best_subpop,opt.n_pop,n_mutations=opt.n_mut)
 else:
+	print('create new generation')
 	population=[]
-	for n in range(N):
+	for n in range(opt.n_pop):
 		population.append(Fungus(n_types=4))
-#-----evolution
-for nc in range(num_competitions):
-	print('----competition: '+str(nc+1))
-	competition(population,idx_map,max_n_iter=max_n_iter)
-	selected_subpop=selection(population,n)
-	print('best score: '+str(selected_subpop[0].score))
-	np_idx_frames=selected_subpop[0].life_frames
-	make_gif(np_idx_frames,path='best_subpop_'+str(nc)+'.gif')
 
-	with open('best_subpop.pkl', 'wb') as f:
+
+#-----evolution
+for nc in range(opt.n_gen):
+	print('----competition: '+str(nc+1))
+	competition(population,idx_map,max_n_iter=opt.n_iter)
+	selected_subpop=selection(population,opt.n_survive)
+	print('best score: '+str(selected_subpop[0].score))
+	print('marginal life probability of best fungus: '+str(selected_subpop[0].p))
+	np_idx_frames=selected_subpop[0].life_frames
+	make_gif(np_idx_frames,path=opt.gif_path+'best_fungus_map='+str(opt.map)+'_gen='+str(nc)+'.gif')
+
+	with open(opt.winner_path+'best_subpop_map='+str(opt.map)+'.pkl', 'wb') as f:
 		pickle.dump(selected_subpop, f )
-	population=mutation(selected_subpop,N,n_mutations=n_mutations)
+	population=mutation(selected_subpop,opt.n_pop,n_mutations=opt.n_mut)
 
 
 #---make gif
-with open('best_subpop.pkl', 'rb') as f:
+with open(opt.winner_path+'best_subpop_map='+str(opt.map)+'.pkl', 'rb') as f:
     best_subpop = pickle.load(f)
 np_idx_frames=best_subpop[0].life_frames
-make_gif(np_idx_frames)
+make_gif(np_idx_frames,path=opt.gif_path+'best_subpop_map='+str(opt.map)+'.gif')
 
 
 
